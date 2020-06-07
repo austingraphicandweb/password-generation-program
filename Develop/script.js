e1df7a800e64c60b82d84e126b555f3bdbaeef10
austinjkeener <austingraphicandweb@gmail.com>
2020-06-07 07:18:25 -0400

    added customizer function to generate a click event

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,21 @@
-// .onClick - prompt length - confirm lowercase - confirm uppercase - confirm numbers - confirm special characters
-// use Crypto.getRandomValues()
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// button click event with prompt and confirm customize options
+// .onClick - prompt length - confirm lowercase - confirm uppercase - confirm numbers - confirm special characters
+function customizer() {
+  prompt("Length of password?");
+  confirm("Uppercase?");
+  confirm("Lowercase?");
+  confirm("Numbers?");
+  confirm("Special Characters?");
+  }
+
+  //variables for custom password specs
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "`", "[", "]", "{", "}", "/",];
 
 // Write password to the #password input
 function writePassword() {
@@ -11,19 +25,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-//variables for custom password specs
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "`", "[", "]", "{", "}", "/",];
-
-// prompt and confirm password specifications
-confirm("Uppercase?");
-confirm("Lowercase?");
-confirm("Numbers?");
-confirm("Special Characters?");
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
