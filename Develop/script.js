8212aa1a3e3d66f839f3952cb23651dc7e592a08
austinjkeener <austingraphicandweb@gmail.com>
2020-06-07 06:53:24 -0400

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+// .onClick - prompt length - confirm lowercase - confirm uppercase - confirm numbers - confirm special characters
+// use Crypto.getRandomValues()
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -5,6 +7,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "`", "[", "]", "{", "}", "/",]
 
   passwordText.value = password;
 
